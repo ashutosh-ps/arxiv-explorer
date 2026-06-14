70fd9f2bf69277470c540c20a88789ce49e84c0b
Ashutosh Pratap Singh <133342016+ashutosh-ps@users.noreply.github.com>
2026-06-15 01:07:02 +0530

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,16 +1,12 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function(app) {
+module.exports = function (app) {
   app.use(
-    '/api',
+    '/api/arxiv',
     createProxyMiddleware({
       target: 'https://export.arxiv.org',
       changeOrigin: true,
-      secure: false,
-      onProxyRes: function (proxyRes, req, res) {
-        proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-      },
-      logLevel: 'debug'
+      pathRewrite: { '^/api/arxiv': '/api/query' },
     })
   );
 };
